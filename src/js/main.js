5d90ae7d911defe73c5fc50e6ac9b5257540c58e
Jacques Uys <jaxuys@gmail.com>
2016-04-27 12:18:53 +0200

    gulp I/O

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+var App = require('./components/App');
+var React = require('react');
+var ReactDOM = require('react-dom');
+var AppAPI = require('./utils/AppAPI.js');
+
+ReactDOM.render(<App />, document.getElementByid('app'));
